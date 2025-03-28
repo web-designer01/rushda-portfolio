64cc49f8b21f95b352c9c897d689f069623b7080
web-designer01 <ali.freelancer.999@gmail.com>
2025-03-29 02:52:43 +0530

    Initial Commit

---
+++ JS main.js
@@ -0,0 +1,7 @@
+// var typed= new typed(".text",{
+//     Strings: ["Frontend Developer", "Youtuber","Web Gingner"],
+//     typespeed:100,
+//     backspeed:100,
+//     backDelay:1000,
+//     loop: true
+// });
